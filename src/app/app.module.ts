eeecf0a62fba7fdc468d039e457a551808a5e931
sriram.j <sriram.j@gofrugal.com>
2018-02-28 14:43:34 +0530

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
+import { AuthGuard } from './shared'
 
 import { AppComponent } from './app.component';
 
@@ -12,9 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutRoutingModule } from './layout-routing.module';
+import { LayoutComponent } from './layout.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        LayoutRoutingModule
+    ],
+    declarations: [LayoutComponent, SidebarComponent, HeaderComponent]
+})
+export class LayoutModule {}
